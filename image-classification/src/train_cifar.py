e2da3f967a1aa7013415358e8ad256587481ee5b
yutayamazaki <tppymd@gmail.com>
2020-01-18 15:41:19 +0900

    Delete and rename

--- image-classification/src/train_cifar.py
+++ image-classification/src/train_cifar.py
@@ -3,21 +3,23 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
+# models.pyで定義したAlexNetをインポートする
 from models import AlexNet
 
+# CIFAR10の10クラスのクラス名
 CLASS_NAMES = (
     'plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog',
     'horse', 'ship', 'truck'
 )
-
+# バッチサイズ，学習率，エポック数，クラス数を定義
 BATCH_SIZE = 256
 LR = 0.01
 NUM_EPOCHS = 50
-
 num_classes = len(CLASS_NAMES)
 
 
 def epoch_train(train_loader, model, optimizer, criterion, device=None):
+    """ モデルを1エポック学習させる関数 """
     if device is None:
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -44,6 +46,7 @@ def epoch_train(train_loader, model, optimizer, criterion, device=None):
 
 
 def epoch_eval(eval_loader, model, criterion, device=None):
+    """ モデルの評価を1エポック分行う関数 """
     if device is None:
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -68,40 +71,46 @@ def epoch_eval(eval_loader, model, criterion, device=None):
 
 
 if __name__ == '__main__':
+    # 画像のリサイズやaugumentationなどの変換処理を学習用とテスト用にそれぞれ定義
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
-
     transform_test = transforms.Compose([
         transforms.Resize(32),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
 
-    trainset = torchvision.datasets.CIFAR10(
+    # CIFAR10のデータセットはtorchvisionを用いてこのように簡単に用意できる
+    dtrain = torchvision.datasets.CIFAR10(
         root='./data', train=True, download=True, transform=transform_train
     )
+    # PyTorchではデータローダークラスを定義して，そこからデータを取り出す形で学習や評価を行う
     train_loader = torch.utils.data.DataLoader(
-        trainset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True
+        dtrain, batch_size=BATCH_SIZE, shuffle=True, drop_last=True
     )
 
-    testset = torchvision.datasets.CIFAR10(
+    dtest = torchvision.datasets.CIFAR10(
         root='./data', train=False, download=True, transform=transform_test
     )
     test_loader = torch.utils.data.DataLoader(
-        testset, batch_size=BATCH_SIZE, shuffle=False
+        dtest, batch_size=BATCH_SIZE, shuffle=False
     )
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    # 損失関数にクロスエントロピー誤差を用いる
     criterion = nn.CrossEntropyLoss()
+    # モデルの定義
     model = AlexNet(num_classes=num_classes)
+    # optimizerはSGD
     optimizer = torch.optim.SGD(model.parameters(), lr=LR, momentum=0.9,
-                          weight_decay=5e-4)
+                                weight_decay=5e-4)
 
+    # 実際の学習部分
     for epoch in range(1, NUM_EPOCHS + 1):
         train_loss, train_acc = epoch_train(train_loader, model, optimizer,
                                             criterion)
@@ -111,5 +120,6 @@ if __name__ == '__main__':
         print(f'TRAIN LOSS: {train_loss:.3f}, TRAIN ACC: {train_acc:.3f}')
         print(f'TEST LOSS: {test_loss:.3f}, TEST ACC: {test_acc:.3f}')
 
+        # このように重みを保存する
         parameters = model.state_dict()
         torch.save(parameters, f'../weights/{epoch}.pth')
